c3f1d90c4d990a187df1cc7373878fa7a81379c2
DianaDI <dianke402@gmail.com>
2017-02-28 17:07:20 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker push dianadi13/sample-node
 
 ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+docker pull dianadi13/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi dianadi13/sample-node:current || true
+docker tag diamadi13/sample-node:latest dianadi13/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 dianadi13/sample-node:current
 EOF
