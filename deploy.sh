7cacfe991fbdd55d600831264f4c7a270985f7b3
DianaDI <dianke402@gmail.com>
2017-03-03 11:19:38 +0300

    fix last

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker build -t dianadi13/sample-node .
 docker push dianadi13/sample-node
 
 ssh dianke402@104.199.12.162 << EOF
@@ -6,6 +7,6 @@ docker pull dianadi13/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi dianadi13/sample-node:current || true
-docker tag diamadi13/sample-node:latest dianadi13/sample-node:current
+docker tag dianadi13/sample-node:latest dianadi13/sample-node:current
 docker run -d --net app --restart always --name web -p 80:80 dianadi13/sample-node:current
 EOF
