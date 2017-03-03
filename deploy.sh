58cdb3b56f2f076391e2c3c1d20488a3014200bf
DianaDI <dianke402@gmail.com>
2017-03-03 11:30:25 +0300

    fox

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 docker push dianadi13/sample-node
 
 ssh dianke402@104.199.12.162 << EOF
@@ -7,5 +8,5 @@ docker stop web || true
 docker rm web || true
 docker rmi dianadi13/sample-node:current || true
 docker tag dianadi13/sample-node:latest dianadi13/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 dianadi13/sample-node:current
+docker run -d --restart always --name web -p 80:80 dianadi13/sample-node:current
 EOF
